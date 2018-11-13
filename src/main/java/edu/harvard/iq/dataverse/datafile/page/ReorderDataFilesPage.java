8c721a6c8d1e42c9c2dcf8616cb82488b50a13dd
Karol Kulik <kkulik@icm.edu.pl>
2018-11-13 16:21:50 +0100

    reordering page is now successfully rendered, added missing functions in reordering page class.

--- src/main/java/edu/harvard/iq/dataverse/datafile/page/ReorderDataFilesPage.java
+++ src/main/java/edu/harvard/iq/dataverse/datafile/page/ReorderDataFilesPage.java
@@ -5,17 +5,22 @@ import edu.harvard.iq.dataverse.DatasetServiceBean;
 import edu.harvard.iq.dataverse.DatasetVersion;
 import edu.harvard.iq.dataverse.FileMetadata;
 import edu.harvard.iq.dataverse.PermissionsWrapper;
+import org.primefaces.event.ReorderEvent;
 
 import javax.ejb.EJB;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @ViewScoped
 @Named("ReorderDataFilesPage")
 public class ReorderDataFilesPage implements java.io.Serializable {
+
+    private static final Logger logger = Logger.getLogger(ReorderDataFilesPage.class.getCanonicalName());
 
     @EJB
     private DatasetServiceBean datasetService;
@@ -24,6 +29,7 @@ public class ReorderDataFilesPage implements java.io.Serializable {
 
     private Dataset dataset = new Dataset();
     private List<FileMetadata> fileMetadatas;
+    private List<Integer> orderedFilesIndex = new ArrayList<>();
 
     /**
      * Initializes all properties requested by frontend.
@@ -32,6 +38,7 @@ public class ReorderDataFilesPage implements java.io.Serializable {
      * @return error if something goes wrong or null if success.
      */
     public String init() {
+        fileMetadatas = new ArrayList<>();
 
         Optional<Dataset> fetchedDataset = fetchDataset(dataset.getId());
 
@@ -40,9 +47,38 @@ public class ReorderDataFilesPage implements java.io.Serializable {
         }
 
         DatasetVersion datasetVersion = fetchedDataset.get().getLatestVersion();
-        fileMetadatas = datasetVersion.getFileMetadatas();
+        fileMetadatas = datasetVersion.getFileMetadatasSorted();
 
         return null;
+    }
+
+    public String saveFileOrder() {
+        fileMetadatas = dataset.getLatestVersion().getFileMetadatasSorted();
+
+        orderedFilesIndex
+                .forEach(orderedFileMeta -> fileMetadatas.stream()
+                        .filter(fileMetadata -> fileMetadata.getDisplayOrder() >= orderedFileMeta)
+                        .forEach(fileMetadata -> fileMetadata.setDisplayOrder(fileMetadata.getDisplayOrder() + 1)));
+
+        return "/dataset.xhtml?persistentId=" + dataset.getGlobalId().asString() + "&version=DRAFT&faces-redirect=true";
+    }
+
+    public String returnToPreviousPage() {
+        return "/dataset.xhtml?persistentId=" + dataset.getGlobalId().asString() + "&version=DRAFT&faces-redirect=true";
+    }
+
+    public void onRowReorder(ReorderEvent event) {
+        int movedToIndex = event.getToIndex();
+
+        logger.info("reorder rows at dataset files");
+        logger.info("From: " + event.getFromIndex() + ", To:" + movedToIndex);
+
+        // already moved in fileMetadataSearch before event wad fired
+        // some get event.getToIndex instead of event.getFromIndex
+
+        orderedFilesIndex.add(movedToIndex);
+
+        //fileMetadataOrderService.reorderExisting(orderedFiles);
     }
 
     /**
